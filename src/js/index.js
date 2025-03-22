65e0fd9cd258428fde9213676a3e6ad34bf8a120
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2025-03-22 16:14:47 +0100

    Initiate switch to tailwind css

--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.getElementById('menuIcon');
-const menuItems = document.getElementById('menuItems');
-
-menuIcon.addEventListener('click', () => {
-  menuItems.classList.toggle('show');
-});
