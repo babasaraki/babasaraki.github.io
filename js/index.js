b93fc0634d199791cf6981e42d8aa3d77c3d0583
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2025-03-22 16:19:53 +0100

    Creates builds for changes made by Dr. Umar

--- js/index.js
+++ js/index.js
@@ -1,17 +1,6 @@
-// Toggle menu items visibility
 const menuIcon = document.getElementById('menuIcon');
 const menuItems = document.getElementById('menuItems');
 
-if (menuIcon && menuItems) {
-  menuIcon.addEventListener('click', () => {
-    menuItems.classList.toggle('show'); // Ensure 'show' is lowercase in CSS
-  });
-}
-
-// Automatically update the copyright year
-document.addEventListener("DOMContentLoaded", function () {
-  const yearElement = document.getElementById("year");
-  if (yearElement) {
-    yearElement.textContent = new Date().getFullYear();
-  }
+menuIcon.addEventListener('click', () => {
+  menuItems.classList.toggle('show');
 });
